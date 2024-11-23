baa05c248475b6492b18050ae4eac9dd74a22996
Estrid4Ever <145568741+Estrid4Ever@users.noreply.github.com>
2024-11-23 20:36:54 +0100

    changed template and style to inline
    changed template and style to inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HeaderComponent],
+  template: `<app-header></app-header>`,
+  styles: ''
 })
 export class AppComponent {
-  title = 'my-portfolio-frontend-angular';
+  title = 'my-portfolio-frontend';
 }
